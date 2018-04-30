9336a05537b6d8cf457b7297e86439483ad5d552
Stepan <omelchenko.stepan@gmail.com>
2018-04-30 23:33:51 +0600

    #2 styles modified

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,10 @@ module.exports = {
                         loader: 'extract-loader'
                     },
                     {
-                        loader: 'css-loader'
+                        loader: 'css-loader',
+                        options: {
+                            minimize: true
+                        }
                     },
                     {
                         loader: 'sass-loader'
