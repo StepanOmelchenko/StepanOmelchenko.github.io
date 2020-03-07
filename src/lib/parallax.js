c104b23524fae2267332e5ea995b17c03b410434
Stepan <omelchenko.stepan@gmail.com>
2020-03-07 21:01:51 +0300

    parallax layers generations to js

--- src/lib/parallax.js
+++ src/lib/parallax.js
@@ -1,13 +1,23 @@
-const layersArr = document.querySelectorAll('.parallax__item');
+const parallax = document.querySelector('.parallax');
+const layers = [];
+const layersCount = 7;
+
+for (let index = 1; index <= layersCount; index++) {
+    const layer = document.createElement('li');
+
+    layer.classList.add('parallax__item', 'parallax__item--layer' + index);
+    layers.push(layer);
+    parallax.appendChild(layer);
+}
 
 window.addEventListener('mousemove', (e) => {
-    let initialX = window.innerWidth / 2 - e.pageX;
-    let initialY = window.innerHeight / 2 - e.pageY;
+    const initialX = window.innerWidth / 2 - e.pageX;
+    const initialY = window.innerHeight / 2 - e.pageY;
 
-    layersArr.forEach((layer, index) => {
-        let divider = index / 100;
-        let positionX = initialX * divider;
-        let positionY = initialY * divider;
+    layers.forEach((layer, index) => {
+        const divider = index / 100;
+        const positionX = initialX * divider;
+        const positionY = initialY * divider;
 
         layer.style.transform = `translate(${positionX}px, ${positionY}px)`;
     });
